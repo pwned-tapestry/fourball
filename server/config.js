16dd6d14dfa986550cc1f62146b2b1f60912bf43
Wayne Muller <github@wainage.com>
2015-02-14 12:27:49 -0800

    Switched mongo databases (for redundancy [sic?]

--- server/config.js
+++ server/config.js
@@ -6,7 +6,8 @@
 module.exports = {
   environment: 'dev',
   mongoDb: {
-    path: 'mongodb://ds050077.mongolab.com:50077/4ball',
+    //path: 'mongodb://ds050077.mongolab.com:50077/4ball',
+    path: 'mongodb://ds041167.mongolab.com:41167/fourball',
     user: 'admin',
     pass: 'fourball'
   }
