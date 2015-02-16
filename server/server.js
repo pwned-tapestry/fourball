fedaac6559947284dd48e8cb7e8704f34624f1a5
Wayne Muller <github@wainage.com>
2015-02-16 10:19:08 -0800

    Added Course Routing for Course API endpoint

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-var mongodb = require('./database/mongoDb');
+var express = require('express');
+var bodyParser = require('body-parser');
+var courseRouter = require('./routes/courseRouter');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public" ));
+
+app.use('/api/course', courseRouter);
+
+
+
+
+app.listen(8080);
