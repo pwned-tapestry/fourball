e1e2ac9a228e332ec4cf07b3de6471e8cacb20e4
Philip Rosen <philipjrosen@gmail.com>
2015-02-18 15:12:41 -0800

    add url property to courses data returned from call to coursesWithTeeTimes--hack until server stores course images

--- mobile/www/services/courses.js
+++ mobile/www/services/courses.js
@@ -9,6 +9,9 @@ function CourseService($http) {
       .success(function(courses, status, headers, config) {
         console.log("Received data via HTTP");
         that._courses = courses;
+        that._courses[0].url = 'http://www.kauaigolfclubrentals.com/wp-content/uploads/2011/09/kiahuna-golf-course-kauai.jpg';
+        that._courses[1].url = 'http://www.clubcorp.com/var/ezflow_site/storage/images/media/clubs/teal-bend-media-folder/images/facilities/golf-course/tealbendgolfclub-hole16-960x410.jpg/3836500-1-eng-US/TealBendGolfClub-Hole16-960x410.jpg_rotatingGalleryFront.jpg';
+        that._courses[2].url = 'http://www.glengolfdesign.com/docs/golf-course-green.jpg';
         callback(courses);
       })
       .error(function(courses, status, headers, config) {
