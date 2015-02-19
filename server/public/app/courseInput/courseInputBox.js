e189427198d409080a073d3569940cf22bd0306d
Nick S <nsipplswezey@gmail.com>
2015-02-19 14:19:01 -0800

    Course input box is now served from public

---
+++ server/public/app/courseInput/courseInputBox.js
@@ -0,0 +1,147 @@
+var Course = React.createClass({
+  render: function() {
+    return (
+      <div className="course">
+      <h2 className="courseAuthor">
+      {this.props.name}
+      </h2>
+
+      <h2 className="courseAddress">{this.props.address}</h2>
+
+
+      {this.props.children}
+      </div>
+    );
+  }
+});
+
+
+var CourseList = React.createClass({
+  render: function(){
+    var courseNodes = this.props.data.map(function(course){
+      return (
+        <Course name={course.name}>
+
+        {course.description}
+        {course.address}
+        </Course>
+      );
+
+    });
+    return (
+      <div className="courseList">
+      {courseNodes}
+      </div>
+    );
+  }
+});
+
+var CourseForm = React.createClass({
+  handleSubmit: function(e){
+    e.preventDefault();
+
+    //Create refs for all input data
+    var name = this.refs.name.getDOMNode().value.trim();
+    var address = this.refs.address.getDOMNode().value.trim();
+    var description = this.refs.description.getDOMNode().value.trim();
+
+    //If no data exists, return
+    if (!name ||  !address || !description) {
+      return;
+    }
+
+    console.log("handleSubmit Called");
+
+    //on submit, we send a json object
+    this.props.onCourseSubmit({name: name, address: address, description: description});
+
+    //Set all dom nodes to an empty string
+    this.refs.name.getDOMNode().value = '';
+    this.refs.address.getDOMNode().value = '';
+    this.refs.description.getDOMNode().value = '';
+
+  },
+
+  render: function(){
+    return(
+      <form className="courseForm" onSubmit={this.handleSubmit}>
+      <input type="text" placeholder="Course name" ref="name"/>
+      <input type="text" placeholder="Location (City, ST)" ref="address"/>
+      <input type="text" placeholder="Description" ref="description"/>
+      <input type="submit" value="Create new course" />
+      </form>
+
+    );
+  }
+});
+
+//api/course/
+//
+//
+
+var CourseBox = React.createClass({
+
+  loadCoursesFromServer: function() {
+    $.ajax({
+      url: "/api/course/",
+      dataType: 'json',
+      success: function(data){
+        console.log(data)
+
+        this.setState({data: data});
+      }.bind(this), //why bind this?
+      //Must be a react thing to set the context of the callback
+      error: function(xhr, status, err){
+        console.error("localhost:8080/api/course/", status, err.toString());
+      }.bind(this)
+    });
+  },
+
+
+  handleCourseSubmit: function(course){
+    //before the ajax request
+    //lets just render the course
+    var courses = this.state.data;
+    var newCourse = courses.concat([course]);
+    this.setState({data: newCourse});
+
+    //the ajax request is still made
+    //So that this gets posted to the database
+    console.log("this is the submitted course", course);
+    console.log("this is what it looks like stringified", JSON.stringify(course));
+
+
+    $.ajax({
+      url: "/api/course/",
+      dataType: 'json',
+      type: 'POST',
+      data: {"name":"TPC", "address":"SF", "description":"Lake Merrit"},
+      success: function(data){
+        console.log("courseSubmit with data:", data);
+        this.setState({data: data})
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.error("localhost:8080/api/course/", status, err.toString())
+      }.bind(this)
+    });
+  },
+
+  getInitialState: function(){
+    return {data: []}
+  },
+
+  componentDidMount: function(){
+    this.loadCoursesFromServer();
+    setInterval(this.loadCoursesFromServer, this.props.pollInterval)
+  },
+
+  render: function(){
+    return(
+      <div className="courseBox">
+      <h1>Open your course for a 4-ball</h1>
+      <CourseList data={this.state.data}/>
+      <CourseForm onCourseSubmit={this.handleCourseSubmit} />
+      </div>
+    );
+  }
+});
