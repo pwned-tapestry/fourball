2f2db318a0976908773ada28abf5b937c7fd7f7a
Philip Rosen <philipjrosen@gmail.com>
2015-02-21 17:00:23 -0800

    adds list of teetimes with filters to limit times displayed to 0 or more times greater or equal to time selected in datepicker

--- mobile/www/app/courses/details/details.js
+++ mobile/www/app/courses/details/details.js
@@ -26,11 +26,18 @@ angular.module('app.courses.details', [])
 .controller('CourseDetailCtrl', function($scope, $stateParams, CourseService) {
 
   $scope.course = CourseService.get($stateParams.courseId);
-
+  $scope.limit = 3;
+  $scope.dateSelected = false;
   $scope.getDateTime = function(event) {
     var datetime = event.target.value;
     var re = /\d{2}:\d{2}/ig;
     var result = re.exec(datetime)[0];
     $scope.teeTime = result.split(':').join("");
+    $scope.dateSelected = true;
   }
+
+  $scope.gteOrEqlSelectTeeTime = function(info){
+    return info.time > $scope.teeTime;
+  }
+
 });
