71af52064a8f294f5566340c7a4167929f22a34c
sdtsui <danielt213@gmail.com>
2015-02-26 14:14:04 -0800

    feats: added demoMap, small tweaks to hrefs and text placement (Limited to P1/P2))

--- mobile/www/app/home/home.js
+++ mobile/www/app/home/home.js
@@ -10,4 +10,44 @@ angular.module('app.home', [])
       }
     }
   });
+})
+
+.controller('MapController', function($scope, $ionicLoading) {
+ // https://blog.nraboy.com/2014/10/implement-google-maps-using-ionicframework/
+    google.maps.event.addDomListener(window, 'load', function() {
+        var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+ 
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+ 
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            var myLocation = new google.maps.Marker({
+                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                map: map,
+                title: "My Location"
+            });
+            // 37.784069, -122.407974
+            var Location1 = new google.maps.Marker({
+                position: new google.maps.LatLng(37.784069, -122.407974),
+                map: map,
+                title: "Marker1"
+            });
+            // 37.782289, -122.410742
+            var Location2 = new google.maps.Marker({
+                position: new google.maps.LatLng(37.782289, -122.410742),
+                map: map,
+                title: "Marker2"
+            });
+
+        });
+ 
+        $scope.map = map;
+    });
+ 
 });
