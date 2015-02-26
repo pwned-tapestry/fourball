81f0bee20d65196e052c6939e6a7cf71f77613eb
sdtsui <danielt213@gmail.com>
2015-02-26 13:44:42 -0800

    fix: Courses sizing

--- mobile/www/app/courses/courses.js
+++ mobile/www/app/courses/courses.js
@@ -25,8 +25,10 @@ angular.module('app.courses', ['app.courses.details'])
 
 .controller('CourseIndexCtrl', function (CourseService) {
   var vm = this;
+  console.log('controller loaded');
   CourseService.getCoursesWithTeeTimes()
   .then(function(courses) {
+    console.log('got some courses');
     vm.courses = courses;
   });
 });
