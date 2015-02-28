f6bba3c536f79aeff4e689a79e8771e4367c2e8d
sdtsui <danielt213@gmail.com>
2015-02-28 15:09:26 -0800

    isolating server folder for heroku

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+ environment: 'dev',
+ mongoDb: {
+   //path: 'mongodb://ds050077.mongolab.com:50077/4ball',
+   path: 'mongodb://ds041167.mongolab.com:41167/fourball',
+   user: 'admin',
+   pass: 'fourball'
+ },
+ twilioKeys: {
+   accountSid: 'AC3c365c4dcab72f00d030ede9555b6d3f',
+   authToken:'18e6fd655f4541b9a84245c418df58d6'
+ }
+};
