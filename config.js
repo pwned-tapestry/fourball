f0818e6c33d1e1b4afc9bcf364594608a8c14c90
sdtsui <danielt213@gmail.com>
2015-03-02 16:47:05 -0800

    fix: new mongolab port, deploy

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
  environment: 'dev',
  mongoDb: {
    //path: 'mongodb://ds050077.mongolab.com:50077/4ball',
-   path: 'mongodb://ds041167.mongolab.com:41167/fourball',
+   path: 'mongodb://ds041167.mongolab.com:45107/fourball',
    user: 'admin',
    pass: 'fourball'
  },
