d149015adfcc676a7cf251971b7c618a7d2544df
sdtsui <danielt213@gmail.com>
2015-03-04 21:17:21 -0800

    test: e2eEndpointTest.js file commented, note: should update name to integrationEndpointTest eventually..

--- test/e2eEndpointTest.js
+++ test/e2eEndpointTest.js
@@ -1,15 +1,18 @@
+//Beginnings of an integration testing suite, for API endpoints.
+
+//Require supertest, for endpoint testing.
 var expect = require('chai').expect;
 var superagent = require('superagent');
 var radConvertor = {
   toMiles : Math.PI*3959/180
 }
 
+//Format a simple query.
 var requestData = {
   miles: 15,
   limit: 5,
   coordinates: [37.783707, -122.408978]
 }
-
 
 describe("e2e: Course endpoints :", function(){
   it("sends a GET to /course/sortedCourses", function(done){
@@ -27,4 +30,4 @@ describe("e2e: Course endpoints :", function(){
     });
 });
 
-})
+});
