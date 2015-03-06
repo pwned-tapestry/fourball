ebe22b7e7ad5aacbab694296654f4312aeb4858e
Philip Rosen <philipjrosen@gmail.com>
2015-03-05 18:59:18 -0800

    remove console.logs, clean up error logs

--- mobile/www/services/courses.js
+++ mobile/www/services/courses.js
@@ -9,17 +9,15 @@ function CourseService($http, $q) {
     var deferred = $q.defer();
     var that = this;
     $http.get("http://localhost:1337/api/coursesWithTeeTimes")
-      .success(function(courses, status, headers, config) {
-        console.log("courses :", courses);
-        console.log("Received data via HTTP");
-        that._courses = courses;
+      .success(function(data, status) {
+        that._courses = data;
         that._courses[0].url = 'http://www.kauaigolfclubrentals.com/wp-content/uploads/2011/09/kiahuna-golf-course-kauai.jpg';
         that._courses[1].url = 'http://www.clubcorp.com/var/ezflow_site/storage/images/media/clubs/teal-bend-media-folder/images/facilities/golf-course/tealbendgolfclub-hole16-960x410.jpg/3836500-1-eng-US/TealBendGolfClub-Hole16-960x410.jpg_rotatingGalleryFront.jpg';
         that._courses[2].url = 'http://www.glengolfdesign.com/docs/golf-course-green.jpg';
-        deferred.resolve(courses);
+        deferred.resolve(data);
       })
-      .error(function(courses, status, headers, config) {
-        console.log("Error while making HTTP call.");
+      .error(function(data, status) {
+        console.log('error :', status);
         deferred.reject();
       });
     return deferred.promise;
@@ -41,27 +39,28 @@ function CourseService($http, $q) {
     //create a new user
     $http.post("http://localhost:1337/api/user", newUser)
 
-    .success(function(returnedUser){
+    .success(function(data, status){
+      deferred.resolve(data);
+
       //then send a text, to that user
-      deferred.resolve(returnedUser);
       $http.post("http://localhost:1337/api/schedule/bookTeeTime", userInfo)
         .success(function(data, status){
-          console.log('success : ', data);
+          console.log('success:', status);
         })
         .error(function(data, status){
-          console.log('error :', data);
+          console.log('error :', status);
         });
       // book the teeTime -- should be combined into one api call with above post to schedule/bookTeeTime
-      $http.post("http://localhost:1337/api/bookteetime",{userId: returnedUser._id, teetimeId: teeTimeId})
-        .success(function(data, status, headers, config) {
-          // console.log("data after post to /bookteetime", data);
+      $http.post("http://localhost:1337/api/bookteetime",{userId: data._id, teetimeId: teeTimeId})
+        .success(function(data, status) {
+          console.log("success:", status);
         })
-        .error(function(data, status, headers, config) {
-          console.log("error:", error);
+        .error(function(data, status) {
+          console.log("error:", status);
         });
     })
-    .error(function(error, status, headers, config) {
-      console.log("error:", error);
+    .error(function(data, status) {
+      console.log("error:", status);
       deferred.reject();
     });
     return deferred.promise;
@@ -69,11 +68,11 @@ function CourseService($http, $q) {
 
   function sendInvites(info) {
     $http.post("http://localhost:1337/api/inviteLinks", info)
-    .success(function(data, status, headers, config) {
-      // console.log("data after post to /inviteLinks", data);
+    .success(function(data, status) {
+      console.log("success:", status);
     })
-    .error(function(error, status, headers, config) {
-      console.log("error:", error);
+    .error(function(data, status) {
+      console.log("error:", status);
     });
   }
 
